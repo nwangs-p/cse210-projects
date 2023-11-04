33f2f9acee3c6ddc862d1030d79bacd599b74f72
nwangs-p <nwa23004@byui.edu>
2023-11-04 15:20:02 -0700

    added a code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+
+       Console.Write("What is your first name? ");
+       string first = Console.ReadLine();
+
+        Console.Write("What is your second name? ");
+        string second = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {first}, {second}");
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1,11);
+
+        int guess = -1;
+
+             while (guess != num)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (num > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (num < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
