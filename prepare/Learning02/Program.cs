9876a28991d8dfe511ff825571632a9d33d2365f
nwangs-p <nwa23004@byui.edu>
2023-11-10 04:47:58 -0800

    learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,81 @@
 using System;
+using System.Collections.Generic;
 
+// Job class
+public class Job
+{
+    // Member variables
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+   
+    public Job(string company, string jobTitle, int startYear, int endYear)
+    {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
+
+    // Display method to show job details
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+// Resume class
+public class Resume
+{
+   
+    public string _personName;
+    public List<Job> _jobs;
+
+    public Resume(string personName)
+    {
+        _personName = personName;
+        _jobs = new List<Job>(); 
+    }
+
+    // Method to add a job to the list
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    // Display method to show resume details
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+
+
+        foreach (var job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
+// Main program
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+  
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Create Resume instance
+        Resume myResume = new Resume("Nanwanga Ifeanyi");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Test Resume Display method
+        myResume.Display();
     }
 }
